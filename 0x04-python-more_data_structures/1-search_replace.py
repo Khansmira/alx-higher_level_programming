adeb154d27f0d14122fcb58ba1d6f20f56dbab69
Khansmira <miriamakanwa@gmail.com>
2023-07-28 23:45:58 +0000

    unction that replaces all occurrences of an element by another in a new list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    """Replaces all occurrences of an element by another in a new list.
+
+    Args:
+        my_list: The initial list.
+        search: The element to replace in the list.
+        replace: The new element.
+
+    Returns:
+        A new list with all occurrences of `search` replaced by `replace`.
+
+    """
+    if my_list is not None:
+        new_list = []
+        for element in my_list:
+            if element == search:
+                new_list.append(replace)
+            else:
+                new_list.append(element)
+        return new_list
