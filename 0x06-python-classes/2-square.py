130b48551281a4353d1ec9da70a1f6c9f5fa1dd5
Khansmira <miriamakanwa@gmail.com>
2023-08-01 20:09:07 +0000

    defines a class Square with private instance Size and function raises appropriate exception errors

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,7 +7,7 @@ private instance attribute: size
 square1 = __import__("1-square").Square
 
 
-class Square(square1):
+class Square(square1.Square):
     """ a class that defines a Square
     Args:
         square1 (class): a class which defines a Square with private attribute size
@@ -19,11 +19,11 @@ class Square(square1):
         Args:
             size: the size of the square passed; set to 0
 
-        exceptions:
+        Errors:
             TypeError: size must be an integer
             ValueError: size must be >= 0
         """
-        square1.__size = size
+        self.__size = size
         self.__size = square1.__size
 
         if type(square1.__size) is not int:
