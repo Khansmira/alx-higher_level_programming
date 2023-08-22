d4734d4e5559bc96207c7313925de884ab820cf6
Khansmira <miriamakanwa@gmail.com>
2023-08-22 05:12:16 +0000

    new task

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i < 99:
-        print("{:02d}, ".format(i), end='')
-    elif i == 99:
-        print("{:02d}".format(i))        
+for i in range(100):
+    print("{:02d}".format(i), end="\n" if i == 99 else ", ")
