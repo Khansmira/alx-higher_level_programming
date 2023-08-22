f3f252ede3f13e780dfd32539e3b82e0d9daba91
Khansmira <miriamakanwa@gmail.com>
2023-08-22 05:47:17 +0000

    new task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+
+
     l = len(sys.argv) - 1
     print("{} argument{}{}"
           .format(l, "" if l == 1 else "s", "." if l == 0 else ":"))
