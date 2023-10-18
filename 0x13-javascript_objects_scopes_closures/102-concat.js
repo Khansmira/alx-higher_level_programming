1c35e01a3fafd8e9fcc8e02289a44c5c5e148598
Khansmira <miriamakanwa@gmail.com>
2023-10-18 09:46:57 -0700

    Add: concatenate files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+const files = require ('fs');
+
+const pathA = process.argv[2];
+const pathB = process.argv[3];
+const pathC = process.argv[4];
+
+function concatFiles (pathA, pathB, pathC) {
+  files.readFile(pathA, 'utf8', (err, dataA) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    files.readFile(pathB, 'utf8', (err, dataB) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      const concatData = dataA + dataB;
+
+      files.writeFile(pathC, concatData, 'utf8', (err) => {
+        if (err) {
+	  console.error(err);
+          return;
+	}
+	console.log(`Files concatenated and saved to ${pathC}`);
+      });
+    });
+  });
+}
