745766590ab5e610d68205645aef53e1a5daf8f8
Khansmira <miriamakanwa@gmail.com>
2023-10-18 11:21:45 -0700

    advanced

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,18 @@
 #!/usr/bin/node
 
-module.exports = class Square extends require('./5-square.js'); {
-  charPrint (c) {
-    if (c === undefined) {
-      this.print();
-    } else {
-      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
-    }
+const Square = require('./5-square.js'); {
+
+class Square extends square {
+  constructor(size) {
+    super(size, size);
+  }
+   charPrint (c) {
+     if (c === undefined) {
+       this.print();
+     } else { 
+       for (let n = 0; n < this.height; n++) console.log(c.repeat(this.width));
+     }
   }
 };
+
+module.exports = Square
