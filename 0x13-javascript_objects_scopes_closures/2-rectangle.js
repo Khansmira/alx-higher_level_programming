2a7b5e7a42942cd18c270f12f1f275d054dc23f1
Khansmira <miriamakanwa@gmail.com>
2023-10-17 19:52:04 -0700

    Add: class Rectangle that defines a rectangle constructor(w, h)

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,9 +2,7 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      return {};
-    } else {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
