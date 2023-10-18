16e8b52db99513ce2545a22f2fb7304c197d0468
Khansmira <miriamakanwa@gmail.com>
2023-10-18 10:22:29 -0700

    Add: concatenate files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const fs = require ('fs');
+const fs = require('fs');
 
 const pathA = fs.readFileSync(process.argv[2], 'utf8');
 const pathB = fs.readFileSync(process.argv[3], 'utf8');
