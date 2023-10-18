d18af0a8c8fa4f2568c4ab95795577d8898a36ef
Khansmira <miriamakanwa@gmail.com>
2023-10-18 07:44:44 -0700

    advanced

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const dict = require('./101-data').dict;
+const newDict = {};
+const userId = Object.entries(dict);
+const occurrence = dict[userId];
+
+if (!newDict[occurrence] {
+  newDict[occurrence] = [];
+}
+newDict[occurrence].push[userId]
+
