f3d5efd8d9de02e91d9fd957fc039961bae32746
Khansmira <miriamakanwa@gmail.com>
2023-10-24 11:09:25 -0700

    Add: script to list database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,11 @@ from sys import argv
 if __name__ == '__main__':
 
     db_connect = MySQLdb.connect(
-            host='localhost', user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+            host='localhost',
+            user=argv[1],
+            port=3306,
+            passwd=argv[2],
+            db=argv[3])
 
     cur = db_connect.cursor()
 
