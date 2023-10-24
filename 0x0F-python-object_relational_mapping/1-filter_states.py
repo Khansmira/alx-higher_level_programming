57f3d87dcd20ed18565e7b1375d18c3c3fe7fd8f
Khansmira <miriamakanwa@gmail.com>
2023-10-24 11:33:44 -0700

    Add: script to list specific database

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,9 +16,10 @@ if __name__ == '__main__':
 
     cur = db_connect.cursor()
 
-    cur.execute('SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC')
+    cur.execute('SELECT id, name FROM states ORDER BY states.id ASC')
 
     rows = cur.fetchall()
 
     for row in rows:
-        print(row)
+        if row[1][0] == 'N':
+            print(row)
