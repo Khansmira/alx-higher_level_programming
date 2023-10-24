f7acbf79da00cf0ede54309dae531bf6f945e55c
Khansmira <miriamakanwa@gmail.com>
2023-10-24 10:50:01 -0700

    Add: script to list database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,9 +3,8 @@
 This script lists all states from the database hbtn_0e_0_usa.
 """
 
-
-from sys import argv
 import MySQLdb
+from sys import argv
 
 if __name__ == '__main__':
     """
@@ -15,7 +14,7 @@ if __name__ == '__main__':
 
     cur = db_connect.cursor()
 
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
 
     rows = cur.fetchall()
 
