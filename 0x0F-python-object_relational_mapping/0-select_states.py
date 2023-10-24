a5227a5159be475e16537f762936165d1c03f677
Khansmira <miriamakanwa@gmail.com>
2023-10-24 11:06:23 -0700

    Add: script to list database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""This script lists all states from the database hbtn_0e_0_usa."""
+""" This script lists all states from the database hbtn_0e_0_usa. """
+
 
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
 
-    db_connect = MySQLdb.connect(host='localhost', user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+    db_connect = MySQLdb.connect(
+            host='localhost', user=argv[1], port=3306, passwd=argv[2], db=argv[3])
 
     cur = db_connect.cursor()
 
