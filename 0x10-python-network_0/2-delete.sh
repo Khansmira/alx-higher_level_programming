f3c54267fd47525fca7a54f019b6f115cfde714b
Khansmira <miriamakanwa@gmail.com>
2023-11-03 05:07:46 +0100

    add: new tasks

---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This bash script sends a DELETE request to an URL with curl and display the body of the response
+curl -sX DELETE "$1"
---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This bash script will send a GET request to the URL using curl, and display the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
